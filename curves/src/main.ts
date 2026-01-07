92d341cd2e1d687c57c7f89a001d0c6146e48046
wsl-11-2023 <lewfar99@gmail.com>
2026-01-07 19:53:37 +0000

    feat: interesting 2d insights...
    I'm learning things :)

--- curves/src/main.ts
+++ curves/src/main.ts
@@ -5,27 +5,45 @@ import * as THREE from "three";
 const canvas = document.querySelector("#bg");
 if (!canvas) { throw new Error("unable to get canvas!") }
 const scene = new THREE.Scene(); 
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight , 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight , 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ canvas });
 
 renderer.setPixelRatio(window.devicePixelRatio); 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-const curve = new THREE.CurvePath<THREE.Vector3>();
-// curve.add( createCurveSC( -4, 0,-5,   0, 0, 3,  -4, 0, 3    ));
-curve.add( createCurve(   -5, 0, 0,   5, 0, 3,   0, 0, 3,   3, 0, -6));
-
-scene.add(new THREE.GridHelper(10, 10));
-
-
 const material = new THREE.PointsMaterial({ size: 0.15, color: new THREE.Color(0,1,0)  });
-const geometry = new THREE.BufferGeometry().setFromPoints(curve.getPoints(10));
-const points = new THREE.Points(geometry, material);
-scene.add(points);
 
-camera.position.set(-8, 5, 8);
+// ===================================================
+// 3D Curve
+// ===================================================
+// const curve = new THREE.CurvePath<THREE.Vector3>();
+// // curve.add( createCurveSC( -4, 0,-5,   0, 0, 3,  -4, 0, 3    ));
+// curve.add( createCurve(   -5, 0, 0,   5, 0, 3,   0, 0, 3,   3, 0, -6));
+// scene.add(new THREE.GridHelper(10, 10));
+// const geometry = new THREE.BufferGeometry().setFromPoints(curve.getPoints(10));
+// const points = new THREE.Points(geometry, material);
+// scene.add(points);
+
+// ===================================================
+// 2D Curve
+// ===================================================
+
+const curve2d = new THREE.CurvePath<THREE.Vector2>();
+
+// START_X,START_Y,  END_X,END_Y,  C1_X,C1_Y, C2_X,C2_Y
+//
+curve2d.add(create2DCurve( -10,0,  10,0,  2.5,10,  -2.5,-10));
+
+const geometry2d = new THREE.BufferGeometry().setFromPoints(curve2d.getPoints(100));
+const points2d = new THREE.Points(geometry2d, material);
+
+scene.add(points2d);
+
+
+// Render
+// camera.position.set(-8, 5, 8);
+camera.position.setZ(10);
 camera.lookAt(0,0,0);
-
 renderer.render(scene, camera);
 
 
@@ -34,6 +52,7 @@ renderer.render(scene, camera);
 // ===================================================
 
 type N = number;
+
 
 // Abstraction over THREE.CubicBezierCurve3
 function createCurve(
@@ -59,4 +78,16 @@ export function createCurveSC(
   return createCurve(sx,sy,sz, ex,ey,ez, c1x,c1y,c1z, c1x,c1y,c1z);
 }
 
+function create2DCurve(
+  sx:N,   sy:N,
+  ex:N,   ey:N,
+  c1x:N,  c1y:N,
+  c2x:N,  c2y:N,
+) {
+  const vStart = new THREE.Vector2(sx,sy);
+  const vEnd = new THREE.Vector2(ex,ey);
+  const c1 = new THREE.Vector2(c1x, c1y);
+  const c2 = new THREE.Vector2(c2x, c2y);
+  return new THREE.CubicBezierCurve(vStart, c1, c2, vEnd);
+}
 
